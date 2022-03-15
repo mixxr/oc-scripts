216d9067a2d76f7740cf97ae2dc708972fb79e8d
Michelangelo Serpico <mise@OSX-MISE-N-IT.local>
2022-03-15 16:13:07 +0100

    webhook pipeline

--- pipeline/pipeline-wh.js
+++ pipeline/pipeline-wh.js
@@ -1,28 +1,49 @@
 export default defineComponent({
-    async run({ steps, $ }) {
-      const headers = {"Content-Type": "application/json"}
-  
-      const xp = {
-        query: steps.trigger.event.query,
-        path: steps.trigger.event.path,
-        event_id: steps.trigger.context.id,
-        workflow_id: steps.trigger.context.workflow_id,
-        owner_id: steps.trigger.context.owner_id,
-        deployment_id: steps.trigger.context.deployment_id,
-        timestamp: steps.trigger.context.ts,
-        inspect: `https://pipedream.com/@/${steps.trigger.context.workflow_id}`, 
-        quickstart: `https://pipedream.com/quickstart/`,
+  async run({ event, steps, $ }) {
+    const headers = { "Content-Type": "application/json" }
+
+    const path = steps.trigger.event.path;
+    const maxQty = 5;
+    let proceedValue = false;
+    let message = "Quantity is not correct value";
+    let suggestion = "Max allowed quantity is " + maxQty;
+    let qty = 0
+
+    try {
+      qty = steps.trigger.event.body.Request.Body.Quantity;
+    } catch (e) { }
+
+    switch (path) {
+      case '/lineqtycheck': {
+        if (qty >= 1 && qty < maxQty) {
+          proceedValue = true
+          message = "Quantity is fine, you can proceed"
+          suggestion = ""
+        }
       }
-  
-      const body = {
-        proceed: true,
-        xp
-      }
-  
-      await $.respond({
-        status: 200,
-        headers,
-        body,
-      })
-    },
-  })
+
+    }
+
+    const xp = {
+      path: path,
+      timestamp: steps.trigger.context.ts,
+      maxQty: maxQty,
+      qty: qty,
+      customMessage: message,
+      suggestions: [
+        suggestion
+      ]
+    }
+
+    const body = {
+      proceed: proceedValue,
+      xp
+    }
+
+    await $.respond({
+      status: 200,
+      headers,
+      body,
+    })
+  },
+})
